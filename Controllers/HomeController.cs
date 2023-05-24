7de10964816280dd3aec2cb9c16ccd7a6ac9b8ed
Daniil Ohorodnik <63656944+SnieFox@users.noreply.github.com>
2023-05-25 02:36:14 +0300

    Добавьте файлы проекта.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,40 @@
+﻿using Memochka.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace Memochka.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult MainPage()
+        {
+            return View();
+        }
+
+        public IActionResult TopMemes()
+        {
+            return View();
+        }
+        public IActionResult MemesHistory()
+        {
+            return View();
+        }
+        public IActionResult MemesOffer()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
