a5cccdedb2d4796ab1b62b005b511ac01d2eeef7
Daniil Ohorodnik <63656944+SnieFox@users.noreply.github.com>
2023-05-26 20:42:47 +0300

    //

---
+++ Models/News.cs
@@ -0,0 +1,18 @@
+﻿namespace Memochka.Models
+{
+    public class News
+    {
+        public int Number { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string Views { get; set; }
+
+        public News(int number, string title, string description, string views)
+        {
+            number = Number;
+            Title = title;
+            Description = description;
+            Views = views;
+        }
+    }
+}
